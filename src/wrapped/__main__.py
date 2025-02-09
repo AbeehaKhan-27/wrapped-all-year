0941c3390755a107865138bce83b859b3efa70b2
Abeeha Khan <ak0212@princeton.edu>
2025-02-09 12:31:19 -0500

    refactor: added get_track_ids and get_track_features functions

--- src/wrapped/__main__.py
+++ src/wrapped/__main__.py
@@ -1,16 +1,31 @@
+import os
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import utility
 
 def main() -> None:
-    load_dotenv()
+    load_dotenv(override=True)
+    SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
+    SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
+    SPOTIPY_REDIRECT_URI = os.getenv('SPOTIPY_REDIRECT_URI')
+    SCOPE = os.getenv('SCOPE')
 
-    auth_manager = SpotifyClientCredentials()
-    sp = spotipy.Spotify(auth_manager=auth_manager)
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id = SPOTIPY_CLIENT_ID,
+                                                   client_secret = SPOTIPY_CLIENT_SECRET,
+                                                   redirect_uri=SPOTIPY_REDIRECT_URI,
+                                                   scope=SCOPE))
 
     playlists = utility.get_user_playlists(sp, "bg7vyk7hmq2vz1hrwcdlhvmbq")
     print(playlists)
+
+    top_tracks_short = sp.current_user_top_tracks(limit=20, offset=0, time_range="short_term")
+    print(top_tracks_short)
+    track_ids = utility.get_track_ids(top_tracks_short)
+    print(track_ids)
+
+    track_id = '4iJyoBOLtHqaGxP12qzhQI'
+    print(utility.get_track_features(sp, track_id))
 
 
 if __name__ == "__main__":
--- src/wrapped/utility.py
+++ src/wrapped/utility.py
@@ -1,7 +1,5 @@
-import spotipy
-
-def get_user_playlists(sp, user: str) -> tuple: #TODO Figure out what dtype this is
-    playlists = sp.user_playlists('')
+def get_user_playlists(sp, user: str) -> tuple:
+    playlists = sp.user_playlists(user)
     while playlists:
         for i, playlist in enumerate(playlists['items']):
             print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
@@ -11,3 +9,18 @@ def get_user_playlists(sp, user: str) -> tuple: #TODO Figure out what dtype this
             playlists = None
     return playlists
 
+def get_track_ids(time_frame):
+    track_ids = []
+    for song in time_frame['items']:
+        track_ids.append(song['id'])
+    return track_ids
+
+def get_track_features (sp, id):
+    meta = sp.track(id)
+    name = meta ['name']
+    album = meta ['album' ] ['name']
+    artist = meta ['album'] ['artists'][0]['name']
+    spotify_url = meta['external_urls']['spotify']
+    album_cover = meta['album']['images'][0]['url']
+    track_info = [name, album, artist, spotify_url, album_cover]
+    return track_info
